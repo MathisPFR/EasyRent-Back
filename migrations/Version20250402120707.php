b00bfe1e9ac1269e6f70ac16ed1d04ceda4a7acf
Sodany <sodany@MacBook-Air-de-Sodany.local>
2025-04-02 14:32:21 +0200

    migration

--- migrations/Version20250402120707.php
+++ migrations/Version20250402120707.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250402073120 extends AbstractMigration
+final class Version20250402120707 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,13 +21,25 @@ final class Version20250402073120 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
+            CREATE TABLE bien (id INT AUTO_INCREMENT NOT NULL, users_id INT NOT NULL, titre VARCHAR(255) NOT NULL, adresse VARCHAR(255) NOT NULL, surface DOUBLE PRECISION NOT NULL, type VARCHAR(255) NOT NULL, loyer DOUBLE PRECISION NOT NULL, INDEX IDX_45EDC38667B3B43D (users_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
             CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE bien ADD CONSTRAINT FK_45EDC38667B3B43D FOREIGN KEY (users_id) REFERENCES `user` (id)
         SQL);
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE bien DROP FOREIGN KEY FK_45EDC38667B3B43D
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE bien
+        SQL);
         $this->addSql(<<<'SQL'
             DROP TABLE `user`
         SQL);
