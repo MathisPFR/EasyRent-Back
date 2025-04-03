a64dbdb6c2919095943cae5c48dd207ae4bf4547
Sodany <sodany@MacBook-Air-de-Sodany.local>
2025-04-03 11:14:35 +0200

    message

--- migrations/Version20250403090615.php
+++ migrations/Version20250403090615.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250402120707 extends AbstractMigration
+final class Version20250403090615 extends AbstractMigration
 {
     public function getDescription(): string
     {
